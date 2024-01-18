f63e68c1d297fa7a7f4495db8c3dfe881585bc56
marlonfolken <marlonvargasmx@gmail.com>
2024-01-18 13:24:04 -0600

    workaround images unoptimized to solve Static HTML export error

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  output: "export"
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+    }
+  }
 }
 
 module.exports = nextConfig
